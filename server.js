9eec3ebdc6cb57262871d39d0dd69b169995921b
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-09-13 16:03:41 +0100

    api v1 working

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/characters/students', function(req, res){
     json = JSON.parse(data);
     students_array = [];
     for(character of json){
-      if(character.hogwartsStudent == true){
+      if(character.isAStudent== true){
       students_array.push(character);
       }
     }
@@ -31,35 +31,35 @@ app.get('/api/characters/students', function(req, res){
   })
 })
 
-app.get('/api/characters/staff', function(req, res){
+app.get('/api/characters/houses', function(req, res){
   fs.readFile(CHARACTERS_JSON, function(err, data){
     if(err) process.exit(1);
     json = JSON.parse(data);
-    staff_array = [];
-    for(character of json){
-      if(character.hogwartsStaff == true){
-      staff_array.push(character);
+    houses_array = [];
+    for(house of json){
+      if(house.isAHouse== true){
+      houses_array.push(house);
       }
     }
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(staff_array);
+    res.json(houses_array);
   })
 })
 
-app.get('/api/characters/house/:house', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
-    json = JSON.parse(data);
-    house_array = [];
-    for(character of json){
-      if(character.house.toLowerCase() == req.params.house.toLowerCase()){
-      house_array.push(character);
-      }
-    }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(house_array);
-  })
-})
+// app.get('/api/characters/house/:house', function(req, res){
+//   fs.readFile(CHARACTERS_JSON, function(err, data){
+//     if(err) process.exit(1);
+//     json = JSON.parse(data);
+//     house_array = [];
+//     for(character of json){
+//       if(character.house.toLowerCase() == req.params.house.toLowerCase()){
+//       house_array.push(character);
+//       }
+//     }
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.json(house_array);
+//   })
+// })
 
 app.use(express.static('public'));
 
